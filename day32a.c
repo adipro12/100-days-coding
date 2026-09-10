6241607f8fb3e70940c93146d9f3ab31302d277b
Aditya <a47729076@gmail.com>
2026-09-10 23:22:37 +0530

    day32

---
+++ day32a.c
@@ -0,0 +1,47 @@
+//Q63: Merge two arrays.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i;
+
+    scanf("%d", &n1);
+
+    int a[n1];
+
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &n2);
+
+    int b[n2];
+
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    // Print first array
+    for(i = 0; i < n1; i++) {
+        printf("%d ", a[i]);
+    }
+
+    // Print second array
+    for(i = 0; i < n2; i++) {
+        printf("%d ", b[i]);
+    }
+
+    return 0;
+}
